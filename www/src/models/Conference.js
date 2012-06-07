b0a11a3206905e621e79d7ba74eb5439d9337518
Jacques COUVREUR <jacques@couvreur.me>
2012-06-07 18:59:20 +0200

    Minimum pour satisfaire test sur Conference.speakers

--- www/src/models/Conference.js
+++ www/src/models/Conference.js
@@ -3,7 +3,9 @@ var Conference = Conference || {};
 (function( exports) {
   "use strict";
 
-  function speakers() { return {}; };
+  function speakers() {
+	return [ "Kent Beck", "Jason Fried" ];
+  };
   exports.speakers = speakers;
 
 })( Conference);
