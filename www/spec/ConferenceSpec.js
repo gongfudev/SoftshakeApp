a1e1b4da91ac209c955cf82972eb0aa110e00806
Jacques COUVREUR <jacques@couvreur.me>
2012-06-07 19:28:50 +0200

    Etoffé test du modèle Conférence pour vérifier disponibilité des attributs des speakers

--- www/spec/ConferenceSpec.js
+++ www/spec/ConferenceSpec.js
@@ -5,9 +5,20 @@ describe("Conference", function() {
     conference = Conference;
   });
 
-  it("should be able to give Speakers", function() {
+  it("should be able to return Speakers", function() {
 	var speakers = conference.speakers();
-    expect(speakers.length).toEqual(2);
-  });
+    expect( speakers.length).toEqual( 2);
 
+	var speaker1 = speakers[ 0];
+    expect( speaker1.id).toEqual( "KentBeck");
+    expect( speaker1.firstname).toEqual( "Kent");
+    expect( speaker1.lastname).toEqual( "Beck");
+    expect( speaker1.URL).toEqual( "http://twitter.com/kentbeck");
+    expect( speaker1.photoURL).toEqual("images/photo-kent-beck.jpg");
+    expect( speaker1.bio).toBeDefined();
+
+	var speaker2 = speakers[ 0];
+    expect( speaker1.id).toEqual( "MartinFowler");
+    expect( speaker1.firstname).toBeDefined();
+  });
 });
--- www/src/models/Conference.js
+++ www/src/models/Conference.js
@@ -3,9 +3,27 @@ var Conference = Conference || {};
 (function( exports) {
   "use strict";
 
+  // var CONFERENCE_DB_FILE = "db-speakers.json";
+  // 
+  // function init() {
+  //   $.getJSON( CONFERENCE_DB_FILE, function( dataJSON) {
+  //     conferenceJSON = dataJSON;
+  // 	  $.trigger("modelLoaded");
+  //   });
+  // };
+
   function speakers() {
 	return [ "Kent Beck", "Jason Fried" ];
   };
+
+
+  // function loadSpeakers(callback) {
+  //   $.getJSON( CONFERENCE_DB_FILE, function( dataJSON) {
+  //     conferenceJSON = dataJSON;
+  // 	  callback(conferenceJSON);
+  //   });
+  // };
+
   exports.speakers = speakers;
 
 })( Conference);
